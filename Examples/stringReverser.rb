9a6b188da6ee70cf872fce627741a5c629da6bb6
mserna <mserna@mail.sfsu.edu>
2018-05-07 21:46:02 -0700

    Add save file unit test to stringReverser file

--- Examples/stringReverser.rb
+++ Examples/stringReverser.rb
@@ -1,5 +1,8 @@
 class StringReverser
-    def reverse_and_save(string_to_reverse)
+    def reverse(string_to_reverse)
         return rev_str = string_to_reverse.reverse
+    end
+    def reverse_and_save(string_to_reverse_and_save)
+        File.write('example_file', string_to_reverse_and_save.reverse)
     end
 end
--- UnitTests/stringReverser_spec.rb
+++ UnitTests/stringReverser_spec.rb
@@ -1,11 +1,22 @@
 require '../Examples/stringReverser'
 
 RSpec.describe StringReverser do
-    it 'reverses strings' do
+    it 'reverses string only' do
         string_reverser = StringReverser.new
 
-        rev_string = string_reverser.reverse_and_save('Hello World!')
+        rev_string = string_reverser.reverse('Hello World!')
 
         expect(rev_string).to eq '!dlroW olleH'
+    end
+
+    it 'saves string to the file system' do
+        string_reverser = StringReverser.new
+        File.stub(:write)
+
+        string_reverser.reverse_and_save('Hello World!')
+        expect(File).
+            to have_received(:write).
+            with('example_file', '!dlroW olleH').
+            once
     end
 end
